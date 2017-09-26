01480c9f098ad2d2fa5a8554f28963bb314a87b2
Hieu Nguyen <hnnguyen45@gmail.com>
2017-09-26 03:38:27 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  // $('#contact-form form').submit(function(e) {
+  //    e.preventDefault();
+  //   var email   = $('#inputEmail').val();
+  //   var message = $('#inputMessage').val();
+
+  //   if (!email || !message) {
+  //     alertify.error("Email and Message are required");
+  //     return false;
+  //   } else {
+  //     $.ajax({
+  //       method: 'POST',
+  //       url: 'http://formspree.io/hnnguyen45@gmail.com',
+  //       data: $('#contact-form form').serialize(),
+  //       datatype: 'json'
+  //     });
+     
+  //     $(this).get(0).reset();
+  //     alertify.success("Message sent");
+  //   }
+  // });
+});
